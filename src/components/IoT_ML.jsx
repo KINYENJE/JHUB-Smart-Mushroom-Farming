2e1da6d20bf5eb75eb599f2777023bc39dcaef09
James Njenga <njengajames232@gmail.com>
2024-05-08 11:22:00 +0300

    iot section mobile responsiveness

--- src/components/IoT_ML.jsx
+++ src/components/IoT_ML.jsx
@@ -4,11 +4,11 @@ function IoT_ML({slides}) {
   return (
     <div className='my-10'>
       {slides.map((slide, index) => (
-        <div key={index} className={`flex ${(slide.name === "Machine Learning" ? "flex-row-reverse h-[480px] border-r-4 border-success" : " h-[440px] border-l-4 border-success")}  w-full my-16 rounded-lg border`}>
-          <div className='w-1/2 flex flex-col gap-3 py-4 px-10'>
+        <div key={index} className={`flex max-sm:flex-col  ${(slide.name === "Machine Learning" ? "flex-row-reverse sm:h-[480px] border-r-4 border-success" : " sm:h-[440px] border-l-4 border-success")}  w-full my-16 rounded-lg border`}>
+          <div className='w-full sm:w-1/2 flex flex-col gap-3 py-4 px-10'>
            <div className='h-1/2'>
             <h3 className='text-2xl border-b-4 border-success'>{slide.name}</h3>
-            <p className='text-left mt-1 text-sm'>{slide.description}</p>
+            <p className='text-left font-semibold text-warmGray-700 mt-1 text-sm'>{slide.description}</p>
            </div>
 
            {slide.subArray.map((sub, index) => (
@@ -20,7 +20,7 @@ function IoT_ML({slides}) {
           
            ))}
           </div>
-          <div className='w-1/2 '>
+          <div className='w-full sm:w-1/2 '>
             <img src={slide.image} alt={slide.name} className={`${(slide.name === "Machine Learning" ? "rounded-r-none" : "rounded-l-none ")} object-cover  object-center h-full w-full rounded-lg`} />
           </div>
         </div>
