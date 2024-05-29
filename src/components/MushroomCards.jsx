a3b657290410e6d821b1faaac08b1f9e3faa7491
James Njenga <njengajames232@gmail.com>
2024-05-29 10:21:53 +0300

    ambient colour change

--- src/components/MushroomCards.jsx
+++ src/components/MushroomCards.jsx
@@ -8,17 +8,17 @@ const MushroomCards = ({cards}) => {
       {cards.map((card, index) => {
         return (
           <CardContainer className="inter-var">
-            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto  h-auto rounded-xl p-6 border  ">
+            <CardBody className=" relative group/card  hover:shadow-2xl hover:shadow-rose-500/[0.1] bg-black border-white/[0.2]  w-auto  h-auto rounded-xl p-6 border  ">
               <CardItem
                 translateZ="50"
-                className="text-xl font-bold text-neutral-600 dark:text-white baloo-tamma-2-bal"
+                className="text-xl font-bold text-white baloo-tamma-2-bal"
               >
                 {card.name}
               </CardItem>
               <CardItem
                 as="p"
                 translateZ="60"
-                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300 baloo-tamma-2-balight text-left"
+                className=" text-sm max-w-sm mt-2 text-neutral-400 baloo-tamma-2-balight text-left"
               >
                 {card.description}
               </CardItem>
@@ -36,16 +36,16 @@ const MushroomCards = ({cards}) => {
                   translateZ={20}
                   as="p"
                  
-                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                  className="px-4 py-2 rounded-xl text-xs font-normal text-white"
                 >
                   Market Price →
                 </CardItem>
                 <CardItem
                   translateZ={20}
                   as="button"
-                  className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                  className="px-4 py-2 rounded-xl  text-black  text-xs font-bold bg-rose-600"
                 >
-                  {card.pricePerKg}
+                  {card.pricePerKg} / <span className='text-xs font-bold baloo-tamma-2-bal'>kg</span>
                 </CardItem>
               </div>
             </CardBody>
