5db2da1827dd2a4dfd6bde03cb7aa3d22e093c23
James Njenga <njengajames232@gmail.com>
2024-06-12 19:59:01 +0300

    video details update

--- src/components/Problem.jsx
+++ src/components/Problem.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import vid1 from "../assets/images/barrel_vid.mp4";
 import vid2 from "../assets/images/sprinkling_vid.mp4";
 import vid3 from "../assets/images/chat_vid.mp4";
@@ -7,12 +7,16 @@ import { LampContainer } from './ui/Lamp';
 
 const Problem = () => {
   const videos = [
-    { title: 'Barrel', src: vid1 },
-    { title: 'Sprinkling', src: vid2 },
-    { title: 'Chat', src: vid3 },
-    { title: 'Bags', src: vid4 },
-
+    { title: 'Barrel', src: vid1 , details: "Chege is rolling a barrel of wheat straw."},
+    { title: 'Sprinkling', src: vid2, details: "Chege is sprinkling fungi spores on the wheat straw."},
+    { title: 'Chat', src: vid3 , details: "Chege is explaining the process of mushroom cultivation."},
+    { title: 'Bags', src: vid4, details: "Chege is packing the mixture of wheat straws and fungi in transparent bags" },
+    { title: 'Bags', src: vid4, details: "Chege is packing the mixture of wheat straws and fungi in transparent bags" },
+    { title: 'Bags', src: vid4, details: "Chege is packing the mixture of wheat straws and fungi in transparent bags" },
+    { title: 'Bags', src: vid4, details: "Chege is packing the mixture of wheat straws and fungi in transparent bags" },
   ];
+
+  const [showDetails, setShowDetails] = useState(videos.map(() => false));
 
   const videoRefs = useRef(videos.map(() => React.createRef(null)));
 
@@ -27,7 +31,9 @@ const Problem = () => {
 
     const observer = new IntersectionObserver((entries) => {
       entries.forEach(entry => {
-        if (entry.target === videoRefs.current[3].current) {
+        // Play the video when it is 100% visible in the viewport
+        // videos after the fourth will only play when visible
+        if (entry.target === videoRefs.current[3].current ) {
           if (entry.isIntersecting) {
             entry.target.play();
           } else {
@@ -35,7 +41,7 @@ const Problem = () => {
           }
         }
       });
-    }, { threshold: 0.5 });
+    }, { threshold: 1.0 });
 
     videoRefs.current.forEach((ref, index) => {
       if (ref.current) {
@@ -67,6 +73,10 @@ const Problem = () => {
     }
   };
 
+  const toggleDetails = (index) => {
+    setShowDetails(prevState => prevState.map((detail, i) => (i === index ? !detail : detail)));
+  };
+
   return (
     <div className='w-full'>
       <h1 className='biryani-light font-thin text-6xl text-white py-4'>
@@ -91,25 +101,32 @@ const Problem = () => {
       <div className='flex flex-col items-center justify-center py-16'>
         <div className='w-full flex gap-7 overflow-auto horiz-scroll'>
           {videos.map((video, index) => (
-            <div className=' rounded-xl  relative '>
+            <div className='rounded-xl relative transition' key={index}>
               {/** on hover a dark overlay with title at top of video */}
-              <div className='bg-black bg-opacity-50 rounded-xl w-full h-full absolute top-0 left-0 flex items-center justify-center'>
-                <p className='biryani-semibold text-white text-2xl'>{video.title}</p>
-              </div>
+                <div className={`bg-black ease-in-out duration-700 bg-opacity-80 ${showDetails[index] ? ' h-[60%]' : ' h-[0%]'} rounded-xl w-full absolute top-0 left-0 flex items-center justify-center z-0`}>
+                  
+                  {showDetails[index] && (
+                    <div className='flex flex-col justify-center items-center gap-24'>
+                      <h1 className='biryani-semibold text-white text-2xl py-8'>{video.title}</h1>
+                      <p className='biryani-semibold text-white text-lg'>{video.details}</p>
+                    </div>
+                  )}
+                </div>
               {/** Video container */}
-              <div className='xl:w-[300px] 2xl:w-[400px] py-2 '>
+              <div className='xl:w-[350px] 2xl:w-[400px] py-2 -z-10'>
                 <video
-                ref={videoRefs.current[index]}
-                onEnded={() => handleVideoEnd(index)}
-                key={index}
-                muted controls playsInline
-                loop // Only loop the videos except the fourth one
-                className='rounded-xl w-full h-full object-cover shadow-lg'
-                >
-                <source src={video.src} type="video/mp4" />
-                Your browser does not support the video tag.
+                  ref={videoRefs.current[index]}
+                  onEnded={() => handleVideoEnd(index)}
+                  key={index}
+                  muted controls playsInline
+                  loop // Only loop the videos except the fourth one
+                  className='rounded-xl w-full h-full object-cover shadow-lg'
+                  >
+                  <source src={video.src} type="video/mp4" />
+                  Your browser does not support the video tag.
                 </video>
               </div>
+              <button onClick={() => toggleDetails(index)} className=' backdrop-blur-xl backdrop-brightness-150 rounded-full py-2 px-3 font-bold text-sm absolute top-3 right-1 z-[100]'>View Details</button>
             </div>
           ))}
         </div>
